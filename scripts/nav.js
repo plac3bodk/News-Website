d2da58ef94975365334d99c2ef0ba6126470ebac
plac3bodk <vince@doomdigital>
2018-07-10 11:31:42 +0200

    Restructured nav

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,15 @@
+//Toggle main navigation
 $(".menuToggle").click(function(){
   $(this).toggleClass("toggled");
   $(".burgerWrap").toggleClass("d");
   $("#nav").toggleClass("navOpen");
   $(".grid").toggleClass("gridPushed");
-})
+});
+
+//Toggle subList
+$(".subNavToggle").each(function(){
+  $(this).click(function(){
+    $(this).toggleClass("d");
+    $(this).parent().find(".subList").toggleClass("navOpen")
+  })
+});
