2a474192568fe3bd38d5cfaeb981322f6c834de9
plac3bodk <vince@doomdigital>
2018-08-16 16:00:26 +0200

    Commented JS

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,23 +1,35 @@
+//Toggle main navigation
+//Click .menuToggle
+$(".menuToggle").click(function() {
+  //Toggle .toggled class on .menuToggle button
+  $(this).toggleClass("toggled");
+  //Toggle .d class on .burgerWrap
+  $(".burgerWrap").toggleClass("d");
+  //Toggle .navOpen class on #nav
+  $("#nav").toggleClass("navOpen");
+  //Toggle .gridPushed class on .grid
+  $(".grid").toggleClass("gridPushed");
+});
 
-  //Toggle main navigation
-  $(".menuToggle").click(function(){
-    $(this).toggleClass("toggled");
-    $(".burgerWrap").toggleClass("d");
-    $("#nav").toggleClass("navOpen");
-    $(".grid").toggleClass("gridPushed");
-  });
+//Toggle subList
+$(".subNavToggle").each(function() {
+  //Click .subNavtoggle + icon
+  $(this).click(function() {
+    //Toggle .d class on .subNavToggle
+    $(this).toggleClass("d");
+    //Find .subList within parent element and toggle .navOpen class
+    $(this).parent().find(".subList").toggleClass("navOpen")
+  })
+});
 
-  //Toggle subList
-  $(".subNavToggle").each(function(){
-    $(this).click(function(){
-      $(this).toggleClass("d");
-      $(this).parent().find(".subList").toggleClass("navOpen")
-    })
-  });
-
-$("#main").click(function(){
+//Close menu when clicking in content window
+//Click content window #main
+$("#main").click(function() {
+  //if #nav element has .navOpen class then...
   if ($("#nav").hasClass("navOpen")) {
+    //...remove .navOpen class from #nav and...
     $("#nav").removeClass('navOpen');
+    //.....remove .gridPushed class from .grid
     $(".grid").removeClass('gridPushed');
   }
 });
--- scripts/readmore.js
+++ scripts/readmore.js
@@ -1,31 +1,33 @@
 //Click on .readMore button
-$(".readMore").click(function(){
-    //Toggle .more on .readMore button
+$(".readMore").click(function() {
+  //Toggle .more on .readMore button
   $(this).toggleClass('more');
-    //Toggle .more on .mainOnpage text area
+  //Toggle .more on .mainOnpage text area
   $(".mainOnpage").toggleClass('more');
-    //If button text equals 'Read more'...
-  if($(this).text() === "Read more"){
-      //...change to 'Read less' else...
+  //If button text equals 'Read more'...
+  if ($(this).text() === "Read more") {
+    //...change to 'Read less' else...
     $(this).text("Read less");
-  }else{
-      //...change back to 'Read more'
+  } else {
+    //...change back to 'Read more'
     $(this).text("Read more");
   }
 });
-    //If .mainOnpage height isn't taller than 135px...
-  if ($(".mainOnpage p").height() < 135 ){
-      //...remove .readMore button
-    $(".readMore").remove();
-  }
+//If .mainOnpage height isn't taller than 135px...
+if ($(".mainOnpage p").height() < 135) {
+  //...remove .readMore button
+  $(".readMore").remove();
+}
 
 //Click on .loadMore button
-$('.loadMore').click(function(){
-    //Add 885px to .#mainPosts container equalling 3 extra lines of posts...
-    $('#mainPosts').animate({height:'+=885'}, 500);
-    //..unless #mainPosts height is over 7950px
-    if ($('#mainPosts').height() > 7950 ) {
-        //..then remove .loadMore button
-      $('.loadMore').remove();
-    }
+$('.loadMore').click(function() {
+  //Add 885px to .#mainPosts container equalling 3 extra lines of posts...
+  $('#mainPosts').animate({
+    height: '+=885'
+  }, 500);
+  //..unless #mainPosts height is over 7950px
+  if ($('#mainPosts').height() > 7950) {
+    //..then remove .loadMore button
+    $('.loadMore').remove();
+  }
 });
